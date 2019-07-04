e12f41087d3230a7784c0c8cfe7f33023e1f1c70
Wuyi Weng <wuweng@microsoft.com>
2019-07-03 21:33:22 -0700

    - move hosting shared project

---
+++ src/Liftr.Hosting/KeyVaultProviderExtension.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Microsoft.Liftr.Hosting
+{
+    public static class KeyVaultProviderExtension
+    {
+        public static IWebHostBuilder UseKeyVaultProvider(this IWebHostBuilder builder, string secretsPrefix)
+        {
+            return builder.ConfigureAppConfiguration((context, config) =>
+            {
+                var builtConfig = config.Build();
+
+                string vaultEndpoint = builtConfig["VaultEndpoint"];
+                if (!string.IsNullOrEmpty(vaultEndpoint))
+                {
+                    string clientId = builtConfig["ClientId"] ?? throw new InvalidOperationException("Please provide AAD ClientId using 'ClientId'");
+                    string clientSecret = builtConfig["ClientSecret"] ?? throw new InvalidOperationException("Please provide AAD ClientSecret using 'ClientSecret'");
+                    config.AddAzureKeyVault(vaultEndpoint, clientId, clientSecret, new PrefixKeyVaultSecretManager(secretsPrefix));
+                }
+            });
+        }
+    }
+}
---
+++ src/Liftr.Hosting/PrefixKeyVaultSecretManager.cs
@@ -0,0 +1,48 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using System;
+
+namespace Microsoft.Liftr.Hosting
+{
+    /// <summary>
+    /// Filter the keys to load by prefix and transform the key name.
+    /// </summary>
+    /// <example>"prefix-Logging--LogLevel--Default" : "Warning"</example>
+    internal class PrefixKeyVaultSecretManager : IKeyVaultSecretManager
+    {
+        private readonly string _prefix;
+
+        public PrefixKeyVaultSecretManager(string prefix)
+        {
+            if (string.IsNullOrEmpty(prefix))
+            {
+                throw new ArgumentNullException(nameof(prefix));
+            }
+
+            _prefix = $"{prefix}-";
+        }
+
+        public bool Load(SecretItem secret)
+        {
+            // Load a vault secret when its secret name starts with the
+            // prefix. Other secrets won't be loaded.
+            return secret.Identifier.Name.StartsWith(_prefix, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public string GetKey(SecretBundle secret)
+        {
+            // Remove the prefix from the secret name and replace two
+            // dashes in any name with the KeyDelimiter, which is the
+            // delimiter used in configuration (usually a colon ':'). Azure
+            // Key Vault doesn't allow a colon (':') in secret names.
+            return secret.SecretIdentifier.Name
+                .Substring(_prefix.Length)
+                .Replace("--", ConfigurationPath.KeyDelimiter);
+        }
+    }
+}
