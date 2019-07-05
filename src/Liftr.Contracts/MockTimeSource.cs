082711b579c89a42df9517c656ee42702e610a6c
Wuyi Weng <wuweng@microsoft.com>
2019-07-05 11:03:01 -0700

    - add time mock

---
+++ src/Liftr.Contracts/MockTimeSource.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+
+namespace Microsoft.Liftr.Contracts
+{
+    public class MockTimeSource : ITimeSource
+    {
+        public DateTime Current { get; set; } = DateTimeOffset.Parse("2019-01-20T08:00:00+00:00", CultureInfo.InvariantCulture).ToUniversalTime().UtcDateTime;
+
+        public DateTime UtcNow => Current;
+
+        public void Add(TimeSpan value)
+        {
+            Current = Current.Add(value);
+        }
+    }
+}
---
+++ src/Liftr.Contracts/SystemTimeSource.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.Liftr.Contracts
+{
+    public class SystemTimeSource : ITimeSource
+    {
+        public DateTime UtcNow
+        {
+            get
+            {
+                return DateTime.UtcNow;
+            }
+        }
+    }
+}
