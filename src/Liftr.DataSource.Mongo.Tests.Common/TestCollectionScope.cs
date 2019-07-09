af82e4c058f5d2f6947af19b62b4c023a9791dce
Wuyi Weng <wuweng@microsoft.com>
2019-07-08 23:46:15 -0700

    - add test common

---
+++ src/Liftr.DataSource.Mongo.Tests.Common/TestCollectionScope.cs
@@ -0,0 +1,45 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Diagnostics;
+
+namespace Microsoft.Liftr.DataSource.Mongo
+{
+    public sealed class TestCollectionScope<T> : IDisposable
+    {
+        private readonly IMongoDatabase _db;
+        private readonly string _collectionName;
+
+        public TestCollectionScope(Func<IMongoDatabase, string, IMongoCollection<T>> collectionCrator)
+        {
+            _db = TestDBConnection.TestDatabase;
+            _collectionName = GenerateRandomCollectionName();
+            Collection = collectionCrator(_db, _collectionName);
+        }
+
+        public IMongoCollection<T> Collection { get; }
+
+        public void Dispose()
+        {
+            try
+            {
+                _db.DropCollection(_collectionName);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+        #region Private methods
+        private static string GenerateRandomCollectionName()
+        {
+            return "test.collection." + ObjectId.GenerateNewId().ToString();
+        }
+        #endregion
+    }
+}
---
+++ src/Liftr.DataSource.Mongo.Tests.Common/TestDBConnection.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using MongoDB.Driver;
+
+namespace Microsoft.Liftr.DataSource.Mongo
+{
+    internal static class TestDBConnection
+    {
+        // TODO: remove this hard-code testing cosmos db.
+        public static string TestMongodbConStr = "bW9uZ29kYjovL25naW54LWRldjpvdHpXa0FIV1NzMGZzVTFnbFVCRkEwS3ZQZXllM1hiMmZ1RHhwUmpDcUp1QWdTbHJOclF3cXdwY2hlRlhLdUt5cmIzZzlTTGVMM0V2MkpsYjE5N25Edz09QG5naW54LWRldi5kb2N1bWVudHMuYXp1cmUuY29tOjEwMjU1Lz9zc2w9dHJ1ZSZyZXBsaWNhU2V0PWdsb2JhbGRi".FromBase64();
+
+        private const string MongodbDatabaseName = "liftr-unit-test";
+
+        public static IMongoClient TestClient { get; } = new MongoClient(TestMongodbConStr);
+
+        public static IMongoDatabase TestDatabase { get; } = TestClient.GetDatabase(MongodbDatabaseName);
+
+        public static MongoOptions TestMongoOptions => new MongoOptions() { ConnectionString = TestMongodbConStr, DatabaseName = MongodbDatabaseName };
+    }
+}
