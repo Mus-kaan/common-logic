0ba398b0d7f4554108d5d4a494a5431baae4d919
Wuyi Weng <wuweng@microsoft.com>
2019-07-08 17:21:16 -0700

    - save

---
+++ src/Liftr.Contracts/ARM/ARMResource.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Microsoft.Liftr.Contracts.ARM
+{
+    public class ARMResource : IARMResource
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public IDictionary<string, string> Tags { get; set; }
+
+        public string Location { get; set; }
+    }
+}
