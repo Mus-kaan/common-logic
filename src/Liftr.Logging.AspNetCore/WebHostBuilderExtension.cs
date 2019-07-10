a989f26d7bb36a7acdb1177ddcf68f900175e0a7
Wuyi Weng <wuweng@microsoft.com>
2019-07-10 10:28:19 -0700

    - add logger initilization.

---
+++ src/Liftr.Logging.AspNetCore/WebHostBuilderExtension.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+
+namespace Microsoft.Liftr.Logging.AspNetCore
+{
+    public static class WebHostBuilderExtension
+    {
+        public static IWebHostBuilder UseLiftrLogger(this IWebHostBuilder webHostBuilder)
+        {
+            if (webHostBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(webHostBuilder));
+            }
+
+            webHostBuilder
+                .UseApplicationInsights()
+                .UseSerilog((host, config) => config.ReadFrom.Configuration(host.Configuration))
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton(Log.Logger);
+                });
+
+            return webHostBuilder;
+        }
+    }
+}
