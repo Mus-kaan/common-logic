96ce424dc4e3f89f6f9f082c2d1290693c3e8aaa
Wuyi Weng <wuweng@microsoft.com>
2019-07-11 11:10:44 -0700

    - filter tests

--- run-windows-tests.cmd
+++ run-windows-tests.cmd
@@ -2,7 +2,7 @@
 
 echo "Starting Windows tests..."
 
-dotnet test %~dp0\src\Liftr.Common.sln --logger:trx --filter "Category!=IntegrationTest" || goto :error
+dotnet test %~dp0\src\Liftr.Common.sln --logger:trx --filter "TraitName!=NeedNetwork" || goto :error
 
 echo "Finished Windows tests successfully"
 goto :EOF
--- src/Liftr.DataSource.Mongo.Tests/ResourceEntityDataSourceTests.cs
+++ src/Liftr.DataSource.Mongo.Tests/ResourceEntityDataSourceTests.cs
@@ -41,6 +41,7 @@ namespace Microsoft.Liftr.DataSource.Mongo.Tests
         }
 
         [Fact]
+        [Trait("NeedNetwork", "Yes")]
         public async Task BasicDataSourceUsageAsync()
         {
             var ts = new MockTimeSource();
