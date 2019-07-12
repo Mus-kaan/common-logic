bd62677c2b88d41821d3208876562587d9f822a0
Wuyi Weng <wuweng@microsoft.com>
2019-07-12 00:30:38 +0000

    Merged PR 1850401: Fix build version issue
    - change to build count.

--- run-windows-build.cmd
+++ run-windows-build.cmd
@@ -5,14 +5,14 @@ echo "Starting Windows build..."
 IF "%CDP_MAJOR_NUMBER_ONLY%"=="" set CDP_MAJOR_NUMBER_ONLY=0
 IF "%CDP_MINOR_NUMBER_ONLY%"=="" set CDP_MINOR_NUMBER_ONLY=1
 IF "%CDP_BUILD_NUMBER%"=="" set CDP_BUILD_NUMBER=1
-IF "%CDP_REVISION_NUMBER%"=="" set CDP_REVISION_NUMBER=2
+IF "%CDP_DEFINITION_BUILD_COUNT%"=="" set CDP_DEFINITION_BUILD_COUNT=2
 
 echo Major version number: %CDP_MAJOR_NUMBER_ONLY%
 echo Minor version number: %CDP_MINOR_NUMBER_ONLY%
 echo Build number: %CDP_BUILD_NUMBER%
-echo Revision number: %CDP_REVISION_NUMBER%
+echo CDP_DEFINITION_BUILD_COUNT: %CDP_DEFINITION_BUILD_COUNT%
 
-dotnet build %~dp0\src\Liftr.Common.sln -c Release --no-restore /p:MajorVersion=%CDP_MAJOR_NUMBER_ONLY% /p:MinorVersion=%CDP_MINOR_NUMBER_ONLY% /p:PatchVersion=%CDP_BUILD_NUMBER% /p:BuildMetadata=%CDP_REVISION_NUMBER% || goto :error
+dotnet build %~dp0\src\Liftr.Common.sln -c Release --no-restore /p:MajorVersion=%CDP_MAJOR_NUMBER_ONLY% /p:MinorVersion=%CDP_MINOR_NUMBER_ONLY% /p:PatchVersion=%CDP_BUILD_NUMBER% /p:BuildMetadata=%CDP_DEFINITION_BUILD_COUNT% || goto :error
 
 echo "Finished Windows build successfully"
 goto :EOF
--- run-windows-pack.cmd
+++ run-windows-pack.cmd
@@ -5,9 +5,9 @@ echo "Packing Windows nugets..."
 echo Major version number: %CDP_MAJOR_NUMBER_ONLY%
 echo Minor version number: %CDP_MINOR_NUMBER_ONLY%
 echo Build number: %CDP_BUILD_NUMBER%
-echo Revision number: %CDP_REVISION_NUMBER%
+echo CDP_DEFINITION_BUILD_COUNT: %CDP_DEFINITION_BUILD_COUNT%
 
-dotnet pack %~dp0src\Liftr.Common.sln -c Release --include-source --include-symbols --no-build --no-restore -o %~dp0nupkgs /p:MajorVersion=%CDP_MAJOR_NUMBER_ONLY% /p:MinorVersion=%CDP_MINOR_NUMBER_ONLY% /p:PatchVersion=%CDP_BUILD_NUMBER% /p:BuildMetadata=%CDP_REVISION_NUMBER% || goto :error
+dotnet pack %~dp0src\Liftr.Common.sln -c Release --include-source --include-symbols --no-build --no-restore -o %~dp0nupkgs /p:MajorVersion=%CDP_MAJOR_NUMBER_ONLY% /p:MinorVersion=%CDP_MINOR_NUMBER_ONLY% /p:PatchVersion=%CDP_BUILD_NUMBER% /p:BuildMetadata=%CDP_DEFINITION_BUILD_COUNT% || goto :error
 
 echo "Finished packing nugets successfully"
 goto :EOF
