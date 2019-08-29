1f29493208b05473ed716700a30f1206f53aaa60
Wuyi Weng <wuweng@microsoft.com>
2019-08-29 19:02:07 +0000

    Merged PR 1994787: Add zulu format datetime converter.
    - add zulu format datetime converter.
    
    PR URL: https://msazure.visualstudio.com/DefaultCollection/Liftr/_git/Liftr.Common/pullrequest/1994787

---
+++ src/Liftr.Utilities.Tests/ZuluDateTimeConverterTests.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using Xunit;
+
+namespace Microsoft.Liftr.Utilities.Tests
+{
+    public class ZuluDateTimeConverterTests
+    {
+        [Fact]
+        public void JsonConverterTest()
+        {
+            var obj = new DateTimeClass();
+            var serialized = JsonConvert.SerializeObject(obj);
+            var recovered = JsonConvert.DeserializeObject<DateTimeClass>(serialized);
+            Assert.True(serialized.IndexOf("Z", StringComparison.OrdinalIgnoreCase) > 0);
+            Assert.Equal(DateTimeKind.Unspecified, recovered.Time1.Kind);
+            Assert.Equal(DateTimeKind.Utc, recovered.Time2.Kind);
+        }
+    }
+
+    public class DateTimeClass
+    {
+        public DateTime Time1 { get; set; } = GetTime();
+
+        [JsonConverter(typeof(ZuluDateTimeConverter))]
+        public DateTime Time2 { get; set; } = GetTime();
+
+        private static DateTime GetTime()
+        {
+            return DateTime.Parse("2019-09-28", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ src/Liftr.Utilities/ZuluDateTimeConverter.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Newtonsoft.Json.Converters;
+using System.Globalization;
+
+namespace Microsoft.Liftr.Utilities
+{
+    public class ZuluDateTimeConverter : IsoDateTimeConverter
+    {
+        public ZuluDateTimeConverter()
+        {
+            DateTimeStyles = DateTimeStyles.AdjustToUniversal;
+        }
+    }
+}
