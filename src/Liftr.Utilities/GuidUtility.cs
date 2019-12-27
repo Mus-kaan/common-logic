8df4edcdb244ee4ca5e3b4dab303868a2ba0a70f
Akshita Agarwal <akagarw@microsoft.com>
2019-12-27 18:59:23 +0000

    Merged PR 2396222: Use Md5 instead of SHA for guid utility
    SHA doesnt produce a byte array of 16 length and hence cannot be used to generate guids. We get a warning that MS5 is a broken algorithm but we are not using this algorithm for any identity critical cryptographic need, we just need to produce deterministic guids for use by the push agent, hence we can safely use md5 here.
    
    PR URL: https://msazure.visualstudio.com/DefaultCollection/Liftr/_git/Liftr.Common/pullrequest/2396222

--- src/Liftr.Utilities/GuidUtility.cs
+++ src/Liftr.Utilities/GuidUtility.cs
@@ -10,12 +10,16 @@ namespace Microsoft.Liftr
 {
     public static class GuidUtility
     {
-        private static readonly SHA256 s_hashGenerator = SHA256.Create();
+        // Utility to generate deterministic guids from strings
+        // http://geekswithblogs.net/EltonStoneman/archive/2008/06/26/generating-deterministic-guids.aspx
+#pragma warning disable CA5351 // Do Not Use Broken Cryptographic Algorithms
+        private static readonly MD5CryptoServiceProvider s_provider = new MD5CryptoServiceProvider();
+#pragma warning restore CA5351 // Do Not Use Broken Cryptographic Algorithms
 
         public static Guid GetDeterministicGuid(string input)
         {
             byte[] inputBytes = Encoding.Default.GetBytes(input);
-            byte[] hashBytes = s_hashGenerator.ComputeHash(inputBytes);
+            byte[] hashBytes = s_provider.ComputeHash(inputBytes);
             Guid hashGuid = new Guid(hashBytes);
             return hashGuid;
         }
