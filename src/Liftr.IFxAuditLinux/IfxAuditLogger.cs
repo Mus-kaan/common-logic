23ec32599e96eba2d45f11f17a6893cd3dea07b4
Limin Gu <limgu@microsoft.com>
2020-05-27 00:28:27 +0000

    Merged PR 2982527: Add IfxAuiditLogger
    Add IfxAuiditLogger
    
    PR URL: https://msazure.visualstudio.com/DefaultCollection/Liftr/_git/Liftr.Common/pullrequest/2982527

---
+++ src/Liftr.IFxAuditLinux/IfxAuditLogger.cs
@@ -0,0 +1,85 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.IFxAudit;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Liftr.IFxAuditLinux
+{
+    public interface IIfxAuditLogger
+    {
+        IFxAuditReturnValue LogAudit(
+            ICollection<IFxAuditCallerId> callerIds,
+            IFxAuditEventCategories eventCategory,
+            string operationName,
+            ICollection<IFxAuditTargetResource> targets,
+            IFxAuditResultType result,
+            string resultDescription = "");
+    }
+
+    public class IfxAuditLogger : IIfxAuditLogger
+    {
+        private static bool s_isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+        static IfxAuditLogger()
+        {
+            if (!s_isLinux)
+            {
+                Console.WriteLine("Not running on Linux. Skipping Microsoft.IFxAudit.");
+                return;
+            }
+
+            var spec = new IFxAudit.IFxAuditSpecification()
+            {
+                Dest = OutputDestination.STDOUT,
+                HeartBeatIntervalSeconds = 60,
+            };
+            IFxAuditReturnValue initValue = IFxAudit.IFxAudit.Initialize(spec);
+
+            if (initValue != IFxAuditReturnValue.AUDIT_SUCCESS)
+            {
+                Console.Error.WriteLine("Microsoft.IFxAudit initialization failed.");
+            }
+            else
+            {
+                Console.WriteLine("Microsoft.IFxAudit is initialized.");
+            }
+        }
+
+        public IfxAuditLogger()
+        {
+        }
+
+        public IFxAuditReturnValue LogAudit(
+            ICollection<IFxAuditCallerId> callerIds,
+            IFxAuditEventCategories eventCategory,
+            string operationName,
+            ICollection<IFxAuditTargetResource> targets,
+            IFxAuditResultType result,
+            string resultDescription = "")
+        {
+            if (!s_isLinux)
+            {
+                return IFxAuditReturnValue.AUDIT_SUCCESS;
+            }
+
+            var reqProperties = new IFxAuditRequiredProperties();
+            reqProperties.CallerIds = callerIds;
+            reqProperties.EventCategories = new IFxAuditEventCategories[]
+            {
+                eventCategory,
+            };
+            reqProperties.OperationName = operationName;
+            reqProperties.ResultType = result;
+            reqProperties.TargetResources = targets;
+
+            var optProperties = new IFxAuditOptionalProperties();
+            optProperties.ResultDescription = resultDescription;
+
+            return IFxAudit.IFxAudit.LogAudit(AuditType.APPLICATION, reqProperties, optProperties);
+        }
+    }
+}
