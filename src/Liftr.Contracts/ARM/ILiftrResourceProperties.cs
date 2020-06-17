35931081699429149855381cda7b8ac18827f0d6
Yu Wang (UC) <yuwa@microsoft.com>
2020-06-17 23:59:33 +0000

    Merged PR 3096680: Add interface for common Liftr ARM properties
    Add interface for common Liftr ARM properties
    
    PR URL: https://msazure.visualstudio.com/DefaultCollection/Liftr/_git/Liftr.Common/pullrequest/3096680
    
    Related work items: #7482147

---
+++ src/Liftr.Contracts/ARM/ILiftrResourceProperties.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+namespace Microsoft.Liftr.Contracts.ARM
+{
+    // ILiftrResourceProperties defines the common properties which should be included in all Liftr resources' ARM property bag
+    public interface ILiftrResourceProperties
+    {
+        // ResourceCategory can be used by ARG query to retrieve all the Liftr resources across all providers.
+        // If the RP doesn't need to be part of any predefined categories, set it to be Unknown
+        public LiftrResourceCategory ResourceCategory { get; set; }
+
+        // ResourcePreference can be used by ARG query to sort the result
+        // By default set it to 0
+        public uint ResourcePreference { get; set; }
+    }
+}
---
+++ src/Liftr.Contracts/LiftrResourceCategory.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Microsoft.Liftr.Contracts
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum LiftrResourceCategory
+    {
+        Unknown = 0,
+        MonitorLogs = 1,
+    }
+}
