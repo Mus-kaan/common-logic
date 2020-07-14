cad01a28f2f68c2623018215a414bfe7ec8c6c07
Anand Kishore <Anand.Kishore@microsoft.com>
2020-07-14 06:33:25 +0000

    Merged PR 3213615: Added complete log method for IFxAudit
    - Added complete log method for IFxAudit
    - Merge branch 'master' into ankisho/IfxAudit_fluentdconfig
    
    PR URL: https://msazure.visualstudio.com/DefaultCollection/Liftr/_git/Liftr.Common/pullrequest/3213615
    
    Related work items: #7614133

---
+++ src/Liftr.IFxAuditLinux/IIfxAuditLogger.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.IFxAudit;
+using System.Collections.Generic;
+
+namespace Microsoft.Liftr.IFxAuditLinux
+{
+    public interface IIfxAuditLogger
+    {
+        IFxAuditReturnValue LogAudit(ICollection<IFxAuditCallerId> callerIds, IFxAuditEventCategories eventCategory, string operationName, ICollection<IFxAuditTargetResource> targets, IFxAuditResultType result, string resultDescription = "");
+
+        IFxAuditReturnValue LogAudit(AuditType auditType, IFxAuditRequiredProperties rqdProps, IFxAuditOptionalProperties optProps = null, IDictionary<string, string> partC = null);
+    }
+}
--- src/Liftr.IFxAuditLinux/IfxAuditLogger.cs
+++ src/Liftr.IFxAuditLinux/IfxAuditLogger.cs
@@ -9,17 +9,6 @@ using System.Runtime.InteropServices;
 
 namespace Microsoft.Liftr.IFxAuditLinux
 {
-    public interface IIfxAuditLogger
-    {
-        IFxAuditReturnValue LogAudit(
-            ICollection<IFxAuditCallerId> callerIds,
-            IFxAuditEventCategories eventCategory,
-            string operationName,
-            ICollection<IFxAuditTargetResource> targets,
-            IFxAuditResultType result,
-            string resultDescription = "");
-    }
-
     public class IfxAuditLogger : IIfxAuditLogger
     {
         private static bool s_isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
@@ -80,6 +69,21 @@ namespace Microsoft.Liftr.IFxAuditLinux
             optProperties.ResultDescription = resultDescription;
 
             return IFxAudit.IFxAudit.LogAudit(AuditType.APPLICATION, reqProperties, optProperties);
+        }
+
+        public IFxAuditReturnValue LogAudit(AuditType auditType, IFxAuditRequiredProperties rqdProps, IFxAuditOptionalProperties optProps = null, IDictionary<string, string> partC = null)
+        {
+            if (!s_isLinux)
+            {
+                return IFxAuditReturnValue.AUDIT_SUCCESS;
+            }
+
+            if (rqdProps is null)
+            {
+                return IFxAuditReturnValue.INVALID_MANDATORY_PROPERTIES;
+            }
+
+            return IFxAudit.IFxAudit.LogAudit(auditType, rqdProps, optProps, partC);
         }
     }
 }
