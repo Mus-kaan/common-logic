002a48caae45b4ab3836db5956f0c1d373deaebe
Wuyi Weng <wuweng@microsoft.com>
2020-07-15 12:38:49 -0700

    - fix cred scan

--- src/Liftr.Encryption.KeyVault.Test/KeyVaultResolverTest.cs
+++ src/Liftr.Encryption.KeyVault.Test/KeyVaultResolverTest.cs
@@ -18,7 +18,7 @@ namespace Liftr.Encryption.KeyVault.Tests
         private readonly string _vaultUrl = "https://logforwarder-df-ms-kv-eu.vault.azure.net/";
         private readonly string _keyName = "TestEncryptionKey1";
         private readonly string _clientId = "_clientId";
-        private readonly string _clientSecret = "_clientSecret";
+        private readonly string _testContent = "testContent";
 
         [Fact(Skip = "Need to set up a test keyvault")]
         public void EncyptionAndDecryption()
@@ -27,14 +27,14 @@ namespace Liftr.Encryption.KeyVault.Tests
             Uri vaultUri = new Uri(_vaultUrl);
 
             // Encryption
-            KeyVaultClient vaultClient = KeyVaultClientFactory.FromClientIdAndSecret(_clientId, _clientSecret);
+            KeyVaultClient vaultClient = KeyVaultClientFactory.FromClientIdAndSecret(_clientId, _testContent);
             var key = KeyVaultEncryptionUtility.SetUpKeyVaultSecretAsync(vaultClient, vaultUri, _keyName, EncryptionAlgorithm.A256CBC).GetAwaiter().GetResult();
             var encryptedResult = KeyVaultEncryptionUtility.EncryptAsync(originalStr, key, EncryptionAlgorithm.A256CBC, CancellationToken.None).GetAwaiter().GetResult();
             var encreptedContent = encryptedResult.Item1;
             var iv = encryptedResult.Item2;
 
             // Decryption
-            KeyVaultKeyResolver keyVaultKeyResolver = KeyVaultKeyResolverFactory.FromClientIdAndSecret(_clientId, _clientSecret);
+            KeyVaultKeyResolver keyVaultKeyResolver = KeyVaultKeyResolverFactory.FromClientIdAndSecret(_clientId, _testContent);
             var kvResolver = new KeyVaultResolver(keyVaultKeyResolver, Logger.None);
             var keyFromKeyVault = kvResolver.ResolveKeyAsSymmetricKeyAsync(key.KeyIdentifier, CancellationToken.None).GetAwaiter().GetResult();
             var decrypted = KeyVaultEncryptionUtility.DecryptAsync(encreptedContent, keyFromKeyVault, iv, EncryptionAlgorithm.A256CBC, CancellationToken.None).GetAwaiter().GetResult();
@@ -49,13 +49,13 @@ namespace Liftr.Encryption.KeyVault.Tests
             Uri vaultUri = new Uri(_vaultUrl);
 
             // Encryption
-            KeyVaultClient vaultClient = KeyVaultClientFactory.FromClientIdAndSecret(_clientId, _clientSecret);
+            KeyVaultClient vaultClient = KeyVaultClientFactory.FromClientIdAndSecret(_clientId, _testContent);
             var key = KeyVaultEncryptionUtility.SetUpKeyVaultSecretAsync(vaultClient, vaultUri, _keyName, EncryptionAlgorithm.A256CBC).GetAwaiter().GetResult();
             var encryptor = new AesEncryptor(key, key.KeyIdentifier, EncryptionAlgorithm.A256CBC);
             var res = encryptor.EncryptAsync(originalStr, CancellationToken.None).GetAwaiter().GetResult();
 
             // Decryption
-            KeyVaultKeyResolver keyVaultKeyResolver = KeyVaultKeyResolverFactory.FromClientIdAndSecret(_clientId, _clientSecret);
+            KeyVaultKeyResolver keyVaultKeyResolver = KeyVaultKeyResolverFactory.FromClientIdAndSecret(_clientId, _testContent);
             var kvResolver = new KeyVaultResolver(keyVaultKeyResolver, Logger.None);
             var keyFromKeyVault = kvResolver.ResolveKeyAsSymmetricKeyAsync(key.KeyIdentifier, CancellationToken.None).GetAwaiter().GetResult();
             var decrypted = KeyVaultEncryptionUtility.DecryptAsync(res.Item1, keyFromKeyVault, res.Item2.ContentEncryptionIV, res.Item2.EncryptionAlgorithm, CancellationToken.None).GetAwaiter().GetResult();
@@ -70,7 +70,7 @@ namespace Liftr.Encryption.KeyVault.Tests
             Uri vaultUri = new Uri(_vaultUrl);
 
             // Encryption
-            KeyVaultClient vaultClient = KeyVaultClientFactory.FromClientIdAndSecret(_clientId, _clientSecret);
+            KeyVaultClient vaultClient = KeyVaultClientFactory.FromClientIdAndSecret(_clientId, _testContent);
             var key = KeyVaultEncryptionUtility.SetUpKeyVaultSecretAsync(vaultClient, vaultUri, _keyName, EncryptionAlgorithm.A256CBC).GetAwaiter().GetResult();
             var encryptor = new AesEncryptor(key, key.KeyIdentifier, EncryptionAlgorithm.A256CBC);
             var res1 = encryptor.EncryptAsync(originalStr, CancellationToken.None).GetAwaiter().GetResult();
