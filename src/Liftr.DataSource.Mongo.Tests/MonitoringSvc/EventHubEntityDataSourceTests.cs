4f33d81de88080a0715d74b7768e3ed379711d04
Wuyi Weng <wuweng@microsoft.com>
2020-08-13 09:57:24 -0700

    - run evh on jenkins

--- src/Liftr.DataSource.Mongo.Tests/MonitoringSvc/EventHubEntityDataSourceTests.cs
+++ src/Liftr.DataSource.Mongo.Tests/MonitoringSvc/EventHubEntityDataSourceTests.cs
@@ -35,7 +35,7 @@ namespace Microsoft.Liftr.DataSource.Mongo.Tests.MonitoringSvc
             _collectionScope.Dispose();
         }
 
-        [SkipInOfficialBuild]
+        [JenkinsOnly]
         public async Task BasicDataSourceUsageAsync()
         {
             var ts = new MockTimeSource();
