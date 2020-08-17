26855e7a9ceb571678772994d1869dd2df18518b
Ubuntu <weng5e@vmss-vm88617000000.zaecj2wucotedgb1czr1jhgned.xx.internal.cloudapp.net>
2020-08-17 04:27:54 +0000

    - update install

--- src/BaseImageBuilder/supporting-files/packer-files/bake-image.sh
+++ src/BaseImageBuilder/supporting-files/packer-files/bake-image.sh
@@ -17,7 +17,6 @@ echo "--------------------------------------------------------------------------
 # sudo dpkg --force-all -P azure-baseimage-patch unattended-upgrades
 # sudo rm -Rf /var/log/unattended-upgrades
 
-
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
 echo "[bake-image.sh] Install .NET Core SDK ..."
 echo "Source: https://docs.microsoft.com/en-us/dotnet/core/install/linux-package-manager-ubuntu-1804"
@@ -26,29 +25,51 @@ echo "[bake-image.sh] Start adding Microsoft repository key and feed ..."
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 
-echo "[bake-image.sh] Installing the .NET Core SDK 3.1 ..."
 sudo add-apt-repository universe
 sudo apt-get update
 sudo apt-get install apt-transport-https -y -q
 sudo apt-get update
+
+echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+echo "[bake-image.sh] Installing the .NET Core SDK 3.1 ..."
 sudo apt-get install dotnet-sdk-3.1 -y -q
+
+echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+echo "[bake-image.sh] Installing the .NET Core SDK 2.1 ..."
+sudo apt-get install dotnet-sdk-2.1 -y -q
 
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
 echo "[bake-image.sh] Installing jq ..."
 sudo apt-get install jq -y
 
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
-echo "[bake-image.sh] Installing az cli ..."
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+echo "[bake-image.sh] Installing zip ..."
+sudo apt-get install zip -y
 
-echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
-echo "[bake-image.sh] Installing kubectl ..."
-az aks install-cli
+if [[ -z $(command -v az) ]]; then
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+    echo "[bake-image.sh] Installing az cli ..."
+    curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+else
+    echo "az cli already installed."
+fi
 
-echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
-echo "[bake-image.sh] Installing docker CE ..."
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+if [[ -z $(command -v kubectl) ]]; then
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+    echo "[bake-image.sh] Installing kubectl ..."
+    az aks install-cli
+else
+    echo "kubectl already installed."
+fi
+
+if [[ -z $(command -v docker) ]]; then
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+    echo "[bake-image.sh] Installing docker CE ..."
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+else
+    echo "docker already installed."
+fi
 
 echo "******************************************************************************************"
 echo "[liftr-image-builder] [bake-image.sh | end] Finished backing VM image ..."
