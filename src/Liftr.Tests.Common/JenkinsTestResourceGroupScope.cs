29f4eba1e8d3490005a51b5d215199c51636c00d
Wuyi Weng <wuweng@microsoft.com>
2020-09-20 23:31:16 -0700

    - override subscription Id in Jenkins tests

--- src/Liftr.Tests.Common/JenkinsTestResourceGroupScope.cs
+++ src/Liftr.Tests.Common/JenkinsTestResourceGroupScope.cs
@@ -10,10 +10,15 @@ namespace Microsoft.Liftr
 {
     public sealed class JenkinsTestResourceGroupScope : TestResourceGroupScope
     {
-        public JenkinsTestResourceGroupScope(string baseName, ITestOutputHelper output, EnvironmentType? env = null, [CallerFilePath] string filePath = "", [CallerMemberName] string memberName = "")
+        public JenkinsTestResourceGroupScope(string baseName, ITestOutputHelper output, EnvironmentType? env = null, string subscriptionId = null, [CallerFilePath] string filePath = "", [CallerMemberName] string memberName = "")
             : base(env.HasValue ? $"{env.Value.ShortName()}-{baseName}" : baseName, output, env, filePath, memberName)
         {
-            AzFactory = new LiftrAzureFactory(Logger, JenkinsTestCredentials.TenantId, JenkinsTestCredentials.ObjectId, JenkinsTestCredentials.SubscriptionId, JenkinsTestCredentials.TokenCredential, JenkinsTestCredentials.GetAzureCredentials);
+            if (string.IsNullOrEmpty(subscriptionId))
+            {
+                subscriptionId = JenkinsTestCredentials.SubscriptionId;
+            }
+
+            AzFactory = new LiftrAzureFactory(Logger, JenkinsTestCredentials.TenantId, JenkinsTestCredentials.ObjectId, subscriptionId, JenkinsTestCredentials.TokenCredential, JenkinsTestCredentials.GetAzureCredentials);
         }
     }
 }
