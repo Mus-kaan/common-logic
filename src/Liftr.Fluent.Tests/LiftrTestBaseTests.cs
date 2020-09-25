077152b427c48759cab3e49d44ca3f78173d5a1d
Wuyi Weng <wuweng@microsoft.com>
2020-09-24 21:58:12 -0700

    - add liftr test base for UI tests

---
+++ src/Liftr.Fluent.Tests/LiftrTestBaseTests.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.Liftr.Tests.Common;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Liftr.Fluent.Tests
+{
+    public class LiftrTestBaseTests : LiftrTestBase
+    {
+        [Fact]
+        public void SimpleTest()
+        {
+#pragma warning disable Liftr1005 // Avoid calling System.Threading.Tasks.Task.Wait()
+            Task.Delay(2000).Wait();
+#pragma warning restore Liftr1005 // Avoid calling System.Threading.Tasks.Task.Wait()
+        }
+    }
+}
---
+++ src/Liftr.Tests.Common/LiftrTestBase.cs
@@ -0,0 +1,99 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Liftr.DiagnosticSource;
+using Microsoft.Liftr.Logging;
+using Serilog;
+using System;
+using Xunit.Abstractions;
+
+namespace Microsoft.Liftr.Tests.Common
+{
+    public class LiftrTestBase : IDisposable
+    {
+        private static readonly string s_appInsightsIntrumentationKey = GetInstrumentationKey();
+        private static readonly IDisposable s_httpClientSubscriber = GetHttpCoreDiagnosticSourceSubscriber();
+
+        private TelemetryConfiguration _appInsightsConfig;
+        private DependencyTrackingTelemetryModule _depModule;
+        private TelemetryClient _appInsightsClient;
+
+        public LiftrTestBase(ITestOutputHelper output = null)
+        {
+            var testClass = GetType().Name;
+            GenerateLogger(testClass, output);
+            TimedOperation = Logger.StartTimedOperation(testClass, generateMetrics: true);
+            TimedOperation.SetProperty("TestEnv", "CICD");
+        }
+
+        public ILogger Logger { get; private set; }
+
+        public ITimedOperation TimedOperation { get; private set; }
+
+        public void Dispose()
+        {
+            try
+            {
+                TimedOperation?.Dispose();
+                _appInsightsClient?.Flush();
+                _appInsightsConfig?.Dispose();
+                _depModule?.Dispose();
+                TimedOperation = null;
+                _appInsightsClient = null;
+                _appInsightsConfig = null;
+                _depModule = null;
+            }
+            catch
+            {
+            }
+        }
+
+        private void GenerateLogger(string testClass, ITestOutputHelper output = null)
+        {
+            _appInsightsConfig = new TelemetryConfiguration(s_appInsightsIntrumentationKey);
+            _depModule = new DependencyTrackingTelemetryModule();
+            _depModule.Initialize(_appInsightsConfig);
+            _appInsightsClient = new TelemetryClient(_appInsightsConfig);
+            AppInsightsHelper.AppInsightsClient = _appInsightsClient;
+
+            var loggerConfig = new LoggerConfiguration()
+                .Enrich.WithProperty("TestClassName", testClass)
+                .Enrich.WithProperty("UnitTestessionId", Guid.NewGuid().ToString())
+                .Enrich.WithProperty("UnitTestStartTime", DateTime.UtcNow.ToZuluString())
+                .WriteTo.ApplicationInsights(_appInsightsClient, TelemetryConverter.Events);
+
+            if (output != null)
+            {
+                loggerConfig = loggerConfig.WriteTo.Xunit(output);
+            }
+
+            Logger = loggerConfig.Enrich.FromLogContext().CreateLogger();
+        }
+
+        private static string GetInstrumentationKey()
+        {
+            if (s_httpClientSubscriber == null)
+            {
+                Console.WriteLine($"'{nameof(s_httpClientSubscriber)}' is null.");
+            }
+
+            var ikey = Environment.GetEnvironmentVariable("LIFTR_APPINSIGHTS_IKEY");
+            if (string.IsNullOrEmpty(ikey))
+            {
+                // /subscriptions/eebfbfdb-4167-49f6-be43-466a6709609f/resourcegroups/liftr-dev-wus-rg/providers/microsoft.insights/components/liftr-unittest-wus2-ai
+                ikey = "78b3bb82-b6b7-42bf-93d8-c8ba1ca26331";
+            }
+
+            return ikey;
+        }
+
+        private static IDisposable GetHttpCoreDiagnosticSourceSubscriber()
+        {
+            return new HttpCoreDiagnosticSourceSubscriber(new HttpCoreDiagnosticSourceListener());
+        }
+    }
+}
