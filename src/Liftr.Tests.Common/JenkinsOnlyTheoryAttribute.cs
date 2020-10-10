2b40d3d524b751fd8a77232af536e87076bfe608
Wuyi Weng <wuweng@microsoft.com>
2020-10-10 11:14:35 -0700

    JenkinsOnlyTheoryAttribute

---
+++ src/Liftr.Tests.Common/JenkinsOnlyTheoryAttribute.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Xunit;
+
+namespace Microsoft.Liftr
+{
+    public sealed class JenkinsOnlyTheoryAttribute : TheoryAttribute
+    {
+        public JenkinsOnlyTheoryAttribute()
+        {
+            // Local debug will not skip the unit test.
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                return;
+            }
+
+            if (!TestConstants.IsJenkins())
+            {
+                Skip = "Ignored for NON Jenkins environments.";
+            }
+        }
+    }
+}
