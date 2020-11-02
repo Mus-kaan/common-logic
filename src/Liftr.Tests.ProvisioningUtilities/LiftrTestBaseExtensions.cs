a44612002bca741033990aa03b60a1dd58b895ec
Wuyi Weng <wuweng@microsoft.com>
2020-11-02 11:26:55 -0800

    Expose key vault client in test

--- src/Liftr.Tests.ProvisioningUtilities/LiftrTestBaseExtensions.cs
+++ src/Liftr.Tests.ProvisioningUtilities/LiftrTestBaseExtensions.cs
@@ -2,7 +2,9 @@
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 //-----------------------------------------------------------------------------
 
+using Microsoft.Azure.KeyVault;
 using Microsoft.Liftr.Fluent;
+using Microsoft.Liftr.KeyVault;
 using System;
 
 namespace Microsoft.Liftr.Tests
@@ -22,6 +24,16 @@ namespace Microsoft.Liftr.Tests
             }
 
             return new LiftrAzureFactory(liftrTestBase.Logger, JenkinsTestCredentials.TenantId, JenkinsTestCredentials.ObjectId, subscriptionId, JenkinsTestCredentials.TokenCredential, JenkinsTestCredentials.GetAzureCredentials);
+        }
+
+        public static KeyVaultClient GetKeyVaultClient(this LiftrTestBase liftrTestBase)
+        {
+            if (liftrTestBase == null)
+            {
+                throw new ArgumentNullException(nameof(liftrTestBase));
+            }
+
+            return KeyVaultClientFactory.FromClientIdAndSecret(JenkinsTestCredentials.ClientId, JenkinsTestCredentials.ClientSecret);
         }
     }
 }
