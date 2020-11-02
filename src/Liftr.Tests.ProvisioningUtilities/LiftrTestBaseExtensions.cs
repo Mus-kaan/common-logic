2008b7af8a62c1ccd72d627130536879d26920fd
Wuyi Weng <wuweng@microsoft.com>
2020-11-02 10:50:00 -0800

    Add Test base helper class to expose test credentials

---
+++ src/Liftr.Tests.ProvisioningUtilities/LiftrTestBaseExtensions.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.Liftr.Fluent;
+using System;
+
+namespace Microsoft.Liftr.Tests
+{
+    public static class LiftrTestBaseExtensions
+    {
+        public static LiftrAzureFactory GetLiftrAzureFactory(this LiftrTestBase liftrTestBase, string subscriptionId = null)
+        {
+            if (liftrTestBase == null)
+            {
+                throw new ArgumentNullException(nameof(liftrTestBase));
+            }
+
+            if (string.IsNullOrEmpty(subscriptionId))
+            {
+                subscriptionId = JenkinsTestCredentials.SubscriptionId;
+            }
+
+            return new LiftrAzureFactory(liftrTestBase.Logger, JenkinsTestCredentials.TenantId, JenkinsTestCredentials.ObjectId, subscriptionId, JenkinsTestCredentials.TokenCredential, JenkinsTestCredentials.GetAzureCredentials);
+        }
+    }
+}
