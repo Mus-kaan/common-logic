1a28cee08a6b44cab34151901feb9dba5fbe99b8
Wuyi Weng <wuweng@microsoft.com>
2020-11-02 22:27:19 -0800

    Expose helper method of getting connection strings

---
+++ src/Liftr.Fluent/CosmosDBAccountExtensions.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.Azure.Management.CosmosDB.Fluent;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Liftr
+{
+    public static class CosmosDBAccountExtensions
+    {
+        public static Task<string> GetPrimaryConnectionStringAsync(this ICosmosDBAccount db)
+            => GetConnectionStringAsync(db, "Primary MongoDB Connection String");
+
+        public static Task<string> GetSecondaryConnectionStringAsync(this ICosmosDBAccount db)
+            => GetConnectionStringAsync(db, "Secondary MongoDB Connection String");
+
+        public static Task<string> GetPrimaryReadOnlyConnectionStringAsync(this ICosmosDBAccount db)
+            => GetConnectionStringAsync(db, "Primary Read-Only MongoDB Connection String");
+
+        public static Task<string> GetSecondaryReadOnlyConnectionStringAsync(this ICosmosDBAccount db)
+            => GetConnectionStringAsync(db, "Secondary Read-Only MongoDB Connection String");
+
+        public static async Task<string> GetConnectionStringAsync(this ICosmosDBAccount db, string description)
+        {
+            if (db == null)
+            {
+                throw new ArgumentNullException(nameof(db));
+            }
+
+            var dbConnectionStrings = await db.ListConnectionStringsAsync();
+            var connStr = dbConnectionStrings.ConnectionStrings.FirstOrDefault(c => c.Description.OrdinalEquals(description)).ConnectionString;
+            return connStr;
+        }
+    }
+}
