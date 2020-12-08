7e7bcad201b88453ed7e91f8058230a48ce8f178
Wuyi Weng <wuweng@microsoft.com>
2020-12-08 14:45:38 -0800

    Add cosmos db connection string model

---
+++ src/Liftr.Contracts/CosmosDBConnectionStrings.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+namespace Microsoft.Liftr.Contracts
+{
+    public class CosmosDBConnectionStrings
+    {
+        public string PrimaryConnectionString { get; set; }
+
+        public string SecondaryConnectionString { get; set; }
+
+        public string PrimaryReadOnlyConnectionString { get; set; }
+
+        public string SecondaryReadOnlyConnectionString { get; set; }
+    }
+}
