c59f9b326a3e7683cf74d8a6f8769fd4594e4bc4
Wuyi Weng <wuweng@microsoft.com>
2020-12-08 16:42:06 -0800

    Add partner name

---
+++ src/Liftr.Contracts/LiftrPartnerName.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+namespace Microsoft.Liftr.Contracts
+{
+    public enum LiftrPartnerName
+    {
+        Datadog,
+        Confluent,
+        Elastic,
+        Logz,
+        Nginx,
+    }
+}
