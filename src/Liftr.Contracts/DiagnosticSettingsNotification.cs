8684d6038995a4764735f6fb5bc2781372bed45c
Feng Wu <xuefwu@microsoft.com>
2021-03-27 01:56:45 +0000

    Merged PR 4300222: Added notification model to common
    Added notification model to common
    
    Related work items: #9493366

---
+++ src/Liftr.Contracts/DiagnosticSettingsNotification.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Liftr.Contracts
+{
+    public class DiagnosticSettingsNotification
+    {
+        public DiagnosticSettingsNotification(string eventType, string monitorId, string diagnosticSettingsId, string tenantId)
+        {
+            MonitorId = monitorId;
+            EventType = eventType;
+            DiagnosticSettingsId = diagnosticSettingsId;
+            TenantId = tenantId;
+        }
+
+        public string MonitorId { get; set; }
+
+        public string EventType { get; set; }
+
+        public string DiagnosticSettingsId { get; set; }
+
+        public string TenantId { get; set; }
+    }
+}
