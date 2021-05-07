c58503ee9108870c042d412408d8720365d09faa
Wuyi Weng <wuweng@microsoft.com>
2021-05-07 23:43:38 +0000

    Merged PR 4488172: Add token credential cache
    In LogForwarder, we are pulling about 20 queues. Each queue will has its own queue client. In each queue client, it has a token cache. However, across the 20 + queues, we do not have a cache. This CL is adding a cache layer to reduce the call number to MI endpoint.

---
+++ src/Liftr.Identity/TokenCredentialCache.cs
@@ -0,0 +1,112 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Azure.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Liftr.Identity
+{
+    public class TokenCacheItem
+    {
+        public DateTimeOffset RenewAfter { get; set; }
+
+        public AccessToken Token { get; set; }
+    }
+
+    public sealed class TokenCredentialCache : TokenCredential, IDisposable
+    {
+        private readonly TokenCredential _tokenCredential;
+        private readonly SemaphoreSlim _mutex = new SemaphoreSlim(1, 1);
+        private readonly Dictionary<string, TokenCacheItem> _cache = new Dictionary<string, TokenCacheItem>();
+
+        public TokenCredentialCache(TokenCredential tokenCredential)
+        {
+            _tokenCredential = tokenCredential;
+        }
+
+        public void Dispose()
+        {
+            _mutex.Dispose();
+        }
+
+        public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            var cacheKey = GetCacheKey(requestContext);
+
+            _mutex.Wait(cancellationToken);
+            try
+            {
+                if (TryGetCachedToken(cacheKey, out var cache))
+                {
+                    return cache.Token;
+                }
+
+                var newToken = _tokenCredential.GetToken(requestContext, cancellationToken);
+                UpdateCache(cacheKey, newToken);
+                return newToken;
+            }
+            finally
+            {
+                _mutex.Release();
+            }
+        }
+
+        public async override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            var cacheKey = GetCacheKey(requestContext);
+
+            await _mutex.WaitAsync(cancellationToken);
+            try
+            {
+                if (TryGetCachedToken(cacheKey, out var cache))
+                {
+                    return cache.Token;
+                }
+
+                var newToken = await _tokenCredential.GetTokenAsync(requestContext, cancellationToken);
+                UpdateCache(cacheKey, newToken);
+                return newToken;
+            }
+            finally
+            {
+                _mutex.Release();
+            }
+        }
+
+        private bool TryGetCachedToken(string cacheKey, out TokenCacheItem cache)
+        {
+            cache = null;
+            if (_cache.ContainsKey(cacheKey))
+            {
+                cache = _cache[cacheKey];
+                if (DateTimeOffset.UtcNow < cache.RenewAfter)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void UpdateCache(string cacheKey, AccessToken token)
+        {
+            var ttlMs = (token.ExpiresOn - DateTimeOffset.UtcNow).TotalMilliseconds * 0.4;
+            var renewAfter = DateTimeOffset.UtcNow + TimeSpan.FromMilliseconds(ttlMs);
+            if (renewAfter > token.ExpiresOn)
+            {
+                renewAfter = token.ExpiresOn;
+            }
+
+            _cache[cacheKey] = new TokenCacheItem() { Token = token, RenewAfter = renewAfter };
+        }
+
+        private static string GetCacheKey(TokenRequestContext requestContext)
+        {
+            return string.Join("-", requestContext.Scopes);
+        }
+    }
+}
---
+++ src/Liftr.Queue.Tests/TokenCredentialCacheTests.cs
@@ -0,0 +1,62 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Azure.Storage.Queues;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Liftr.Contracts;
+using Microsoft.Liftr.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Microsoft.Liftr.Queue.Tests
+{
+    public class TokenCredentialCacheTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public TokenCredentialCacheTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [CheckInValidation(skipLinux: true)]
+        public async Task CacheCanUsedForQueueAsync()
+        {
+            var ts = new MockTimeSource();
+            using (var semaphore = new SemaphoreSlim(0, 1))
+            using (var scope = new TestResourceGroupScope(SdkContext.RandomResourceName("q", 15), _output))
+            {
+                try
+                {
+                    var storageAccount = await scope.GetTestStorageAccountAsync();
+                    using var tokenCache = new TokenCredentialCache(TestCredentials.TokenCredential);
+
+                    for (int i = 0; i < 20; i++)
+                    {
+                        var queueUri = new Uri($"https://{storageAccount.Name}.queue.core.windows.net/myqueue" + i);
+                        QueueClient queue = new QueueClient(queueUri, tokenCache);
+                        await queue.CreateIfNotExistsAsync();
+                        await queue.SendMessageAsync("test message");
+                    }
+
+                    for (int i = 0; i < 20; i++)
+                    {
+                        var queueUri = new Uri($"https://{storageAccount.Name}.queue.core.windows.net/syncmyqueue" + i);
+                        QueueClient queue = new QueueClient(queueUri, tokenCache);
+                        queue.CreateIfNotExists();
+                        queue.SendMessage("test message");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    scope.Logger.Error(ex, "Failed.");
+                    scope.TimedOperation.FailOperation(ex.Message);
+                    throw;
+                }
+            }
+        }
+    }
+}
