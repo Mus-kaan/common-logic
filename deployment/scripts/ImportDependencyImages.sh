2bda827462cb16e49699d063e7c956ed299f5b8f
Limin Gu <limgu@microsoft.com>
2021-05-18 23:01:30 +0000

    Merged PR 4533172: Update geneva images
    Update geneva images

--- deployment/scripts/ImportDependencyImages.sh
+++ deployment/scripts/ImportDependencyImages.sh
@@ -12,10 +12,10 @@ fi
 # The version are referenced at three places. You need to update all of them. Please search for this sentence.
 # latest prom mdm image version: https://msazure.visualstudio.com/Liftr/_build?definitionId=113171&_a=summary&view=runs
 echo "Latest geneva image versions: https://genevamondocs.azurewebsites.net/collect/references/linuxcontainers.html"
-IMG_mdsd="genevamdsd:master_20210401.1"
-IMG_mdm="genevamdm:master_20210401.1"
-IMG_fluentd="genevafluentd_td-agent:master_20210401.1"
-IMG_azsecpack="genevasecpackinstall:master_20210401.1"
+IMG_mdsd="genevamdsd:master_20210513.2"
+IMG_mdm="genevamdm:master_20210505.1"
+IMG_fluentd="genevafluentd_td-agent:master_20210505.1"
+IMG_azsecpack="genevasecpackinstall:master_20210505.1"
 IMG_prommdm="shared/prom-mdm-converter:2.0.master.20210311.1"
 
 for i in "$@"
