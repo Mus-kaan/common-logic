620f0a39bda443f2ceb5b5bbd8d17cb94bbfecb7
Wuyi Weng <wuweng@microsoft.com>
2021-06-14 08:18:43 -0700

    Add test result aggregator project

---
+++ src/Liftr.TestResultAggregator/Program.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Liftr.Logging.AspNetCore;
+using Microsoft.Liftr.Metrics.Prom;
+
+namespace Microsoft.Liftr.TestResultAggregator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebHost
+                .CreateDefaultBuilder(args)
+                .UseLiftrLogger()
+                .UsePrometheusMetrics()
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+        }
+    }
+}
---
+++ src/Liftr.TestResultAggregator/WeatherForecast.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.Liftr.TestResultAggregator
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
