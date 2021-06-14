7079882888a495a02a6a479916a3828d353af813
Wuyi Weng <wuweng@microsoft.com>
2021-06-14 05:55:56 +0000

    add more test details

--- build/jenkins-test-PublicWestUS2.sh
+++ build/jenkins-test-PublicWestUS2.sh
@@ -5,7 +5,7 @@ echo "Starting tests..."
 SrcRoot="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 echo "SrcRoot $SrcRoot"
 
-dotnet test $SrcRoot/src/Liftr.Fluent.Tests/Liftr.Fluent.Tests.csproj --filter RegionCategory=PublicWestUS2 --logger:trx
+dotnet test $SrcRoot/src/Liftr.Fluent.Tests/Liftr.Fluent.Tests.csproj --no-build --filter RegionCategory=PublicWestUS2 --logger:trx -v:diag
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
     echo "Failed to test"
