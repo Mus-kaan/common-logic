3237759787784808b2be80311c0dbfe32726b58d
Wuyi Weng <wuweng@microsoft.com>
2021-06-20 09:13:20 -0700

    Add sample worker project for testing

---
+++ src/Samples/Liftr.Sample.Worker/Program.cs
@@ -0,0 +1,27 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Liftr.Logging.AspNetCore;
+using Microsoft.Liftr.Metrics.Prom;
+using Microsoft.Liftr.WebHosting;
+
+namespace Liftr.Sample.Worker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebHost
+                .CreateDefaultBuilder(args)
+                .UseLiftrLogger()
+                .UsePrometheusMetrics()
+                .UseKeyVaultProvider("SampleRP")
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+        }
+    }
+}
