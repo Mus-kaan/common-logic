14ccf5d614759defe6ca574ef434c814d4381a85
Kanupriya Singh <kansingh@microsoft.com>
2021-12-07 06:30:33 +0000

    Merged PR 5436439: Added Patch RPaaS Object Message
    Added Patch RPaaS Object Message
    
    Related work items: #12684953

---
+++ src/Liftr.ACIS.Contracts/PatchRPaaSObjectMessage.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+namespace Microsoft.Liftr.ACIS.Contracts
+{
+    public class PatchRPaaSObjectMessage
+    {
+        /// <summary>
+        /// The resource Id of the monitor resource.
+        /// </summary>
+        public string MonitorId { get; set; }
+
+        /// <summary>
+        /// The RPaaS object.
+        /// </summary>
+        public string RPaaSobject { get; set; }
+    }
+}
