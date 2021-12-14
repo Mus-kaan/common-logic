3bba85acb2ec884672dad05729a909bc6d9abed2
Kanupriya Singh <kansingh@microsoft.com>
2021-12-14 06:04:07 +0000

    Merged PR 5470141: Added Update Monitor Creation Status Message
    Added Update Monitor Creation Status Message
    
    Related work items: #12684955

---
+++ src/Liftr.ACIS.Contracts/UpdateMonitorCreationStatusMessage.cs
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+namespace Microsoft.Liftr.ACIS.Contracts
+{
+    public class UpdateMonitorCreationStatusMessage
+    {
+        /// <summary>
+        /// The resource Id of the monitor resource.
+        /// </summary>
+        public string MonitorId { get; set; }
+
+        /// <summary>
+        /// The monitor creation status object.
+        /// </summary>
+        public string MonitorCreationStatusObject { get; set; }
+
+        /// <summary>
+        /// Option for API token update.
+        /// </summary>
+        public bool APIToken { get; set; }
+
+        /// <summary>
+        /// Option for shipping token update.
+        /// </summary>
+        public bool ShippingToken { get; set; }
+
+        /// <summary>
+        /// Option for marketplace resource Id update.
+        /// </summary>
+        public bool MarketplaceResourceId { get; set; }
+    }
+}
