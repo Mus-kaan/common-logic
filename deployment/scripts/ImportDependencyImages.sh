7af0d4921b3636046c71797a114cf4b1e227a243
Shivam Singh <shivamsingh@microsoft.com>
2022-02-04 15:37:38 +0000

    Merged PR 5658551: Update to latest version of container images for Geneva Components
    Update to latest version of container images for Geneva Components
    
    Related work items: #13175167

--- deployment/scripts/ImportDependencyImages.sh
+++ deployment/scripts/ImportDependencyImages.sh
@@ -12,11 +12,11 @@ fi
 # The version are referenced at two places. You need to update all of them. Please search for this sentence.
 # latest prom mdm image version: https://msazure.visualstudio.com/Liftr/_build?definitionId=113171&_a=summary&view=runs
 echo "Latest geneva image versions: https://genevamondocs.azurewebsites.net/collect/references/linuxcontainers.html"
-IMG_mdsd="genevamdsd:master_20211223.1"
-IMG_mdm="genevamdm:master_20220111.2"
-IMG_fluentd="genevafluentd_td-agent:master_20220121.1"
-IMG_azsecpack="genevasecpackinstall:master_20211102.2"
-IMG_prommdm="shared/prom-mdm-converter:2.0.master.20211213.1"
+IMG_mdsd="genevamdsd:master_20220202.1"
+IMG_mdm="genevamdm:master_20220202.1"
+IMG_fluentd="genevafluentd_td-agent:master_20220202.1"
+IMG_azsecpack="genevasecpackinstall:master_20220202.1"
+IMG_prommdm="shared/prom-mdm-converter:2.0.master.20220204.1"
 
 for i in "$@"
 do
