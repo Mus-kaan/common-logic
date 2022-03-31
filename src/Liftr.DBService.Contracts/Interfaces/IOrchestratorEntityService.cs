f0a590f8108b7c0218be15e1ee5407f997208e47
Shivam Singh <shivamsingh@microsoft.com>
2022-03-31 10:33:56 +0000

    Merged PR 5898921: Add common interface for RestSharp service
    Add common interface for RestSharp service

---
+++ src/Liftr.DBService.Contracts/Interfaces/IOrchestratorEntityService.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace Microsoft.Liftr.DBService.Contracts.Interfaces
+{
+    // This needs to be moved to liftr common
+    public interface IOrchestratorEntityService<T>
+    {
+        Task<T> CreateOrchestratorEntityAsync(T entity);
+
+        Task<T> GetOrchestratorEntityAsync(string serviceTreeId, string repoName);
+
+        Task<T> UpdateOrchestratorEntityAsync(T entity);
+    }
+}
---
+++ src/Liftr.DBService.Contracts/Interfaces/IRestSharpService.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.Liftr.DBService.Contracts.Interfaces
+{
+    public interface IRestSharpService<TEntity, TResult>
+    {
+        Task<TResult> CreateAsync(TEntity entity, IDictionary<string, string> headers, Uri endpoint);
+
+        Task<TResult> GetAsync(IDictionary<string, string> headers, Uri endpoint);
+
+        Task<TResult> UpdateAsync(TEntity entity, IDictionary<string, string> headers, Uri endpoint);
+    }
+}
