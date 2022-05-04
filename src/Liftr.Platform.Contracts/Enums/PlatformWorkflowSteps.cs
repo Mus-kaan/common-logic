9bf44837fdcfc7987e5c874920a4b493d11c7d71
Shivam Singh <shivamsingh@microsoft.com>
2022-05-04 08:15:51 +0000

    Merged PR 6024661: Added PlatformWorkflowSteps.cs
    Added PlatformWorkflowSteps.cs

---
+++ src/Liftr.Platform.Contracts/Enums/PlatformWorkflowSteps.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//-----------------------------------------------------------------------------
+
+namespace Microsoft.Liftr.Platform.Contracts.Enums
+{
+    public enum PlatformWorkflowSteps
+    {
+        ADO = 0,
+        AppCodeGen = 1,
+        ADOPolicy = 2,
+        BuildPipelines = 3,
+        ReleasePipelines = 4,
+    }
+}
